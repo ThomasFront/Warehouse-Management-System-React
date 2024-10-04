113faeb024438ed7a72e4f63241c29f93f6d2862
Tomasz Brandys <cyzmenpl@gmail.com>
2024-10-04 18:28:58 +0200

    create sales history list

--- src/pages/Sales/SalesHistory/SalesHistory.tsx
+++ src/pages/Sales/SalesHistory/SalesHistory.tsx
@@ -1,12 +1,25 @@
 import { Box } from "@mui/material"
+import { useTranslation } from "react-i18next"
 import { PageInfo } from "../../../components/PageInfo"
+import { DataGrid } from "../../../components/DataGrid"
+import { saleColumns } from "./columns"
 
 export const SalesHistory = () => {
+  const { t } = useTranslation()
+
+  const saleColumnsWithActions = [
+    ...saleColumns(t)
+  ]
+
   return (
     <Box>
       <PageInfo
         title="Sales history"
         subtitle="On this page you can view your complete sales history in an easy-to-read table. We also offer the ability to export data, which facilitates further analysis and archiving"
+      />
+      <DataGrid
+        endpoint="sales"
+        columns={saleColumnsWithActions}
       />
     </Box>
   )
---
+++ src/pages/Sales/SalesHistory/columns.tsx
@@ -0,0 +1,58 @@
+import { GridRenderCellParams } from "@mui/x-data-grid";
+import { TFunction } from "i18next";
+import { formatDateTimeToDisplay, transformAvatarStorageUrl } from "../../../utils/common";
+import productDefaultImage from "../../../assets/images/productDefault.png"
+import { Avatar, Box } from "@mui/material";
+
+export const saleColumns = (t: TFunction) => ([
+  {
+    field: "id",
+    headerName: "ID",
+    width: 100
+  },
+  {
+    field: "productName",
+    headerName: t("Product"),
+    width: 200,
+    filterable: false,
+    sortable: false,
+    renderCell: ({ row }: GridRenderCellParams) => {
+      const image = row.product.image
+      const productImage = image ? `${import.meta.env.VITE_BACKEND_LARAVEL}/${transformAvatarStorageUrl(image)}` : productDefaultImage
+
+      return (
+        <Box display="flex" alignItems="center" gap={1}>
+          <Avatar src={productImage} />
+          {row.product.name}
+        </Box>
+      )
+    }
+  },
+  {
+    field: "quantity",
+    headerName: t("Quantity"),
+    width: 200
+  },
+  {
+    field: "salePrice",
+    headerName: t("Sales price"),
+    width: 200,
+    filterable: false,
+    sortable: false,
+    renderCell: ({ row }: GridRenderCellParams) => `${row.product.price} PLN`
+  },
+  {
+    field: "totalPrice",
+    headerName: t("Total price"),
+    width: 200,
+    filterable: false,
+    sortable: false,
+    renderCell: ({ row }: GridRenderCellParams) => `${row.totalPrice} PLN`
+  },
+  {
+    field: "createdAt",
+    headerName: t("Created at"),
+    width: 200,
+    renderCell: ({ row }: GridRenderCellParams) => formatDateTimeToDisplay(row.createdAt)
+  },
+])
